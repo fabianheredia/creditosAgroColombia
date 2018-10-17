1378709482e479cefbf2416ca46344e01b59df27
fabian <fabian.heredia@gmail.com>
2018-10-17 16:09:41 -0500

    mejoras no funcional

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# creditosAgroColombia
-Analisis a los creditos otorgados por el Banco Agrario a grandes y pequenos productores  para el periodo de 2012 a 2018
+# Distribucion de Creditos al Agro en Colombia
+
+ Analisis a la información sobre los desembolsos realizados por el Banco Agrario y Finagro en cada departamento durante el período 2012 a 2018.
+
+
+         # Como Instalar
+clonar o descargar el repositorio
+ ``` git clone [repo] ```
+copiar y pegar el contenido en un servidor de aplicaciones
+* tomcat
+* IIS
+* ...
+
+ejecutar la aplicación desde el explorador
+
+# Tecnologias usadas
+* D3 V5
+* BulmaJs
+
+# **_Insight:_** :shipit:
+* 
+
+# De donde vienen los Datos
+
+Los datos provienen de [Agronet](http://www.agronet.gov.co/estadistica/Paginas/default.aspx) y son suministrados por **Banco Agrario y Finagro**
+
+# What
+Dataset Availability: Static
+
+Data Types: Table -> Items -> Attributes
+
+Data and Dataset Types: Temporal
+
+Dataset Types: Temporal
+
+_Attributes:_
+
+| item   |      Type      |  Description |
+|----------|:-------------:|------:|
+| Tproductor |  Categorical (Pequeño Productor; Grandes Productores) | Son los productores a los que el Bango Agrario y Finagro han otorgado creditos |
+| Departamento |    Categorical (Todos los Departamentos de Colombia)   |   Division politica de Colombia |
+| Anio | Time (Ordered -> Quantitative -> Sequential) |    Variable de tiempo que indica el periodo en el cual fue otorgado el credito |
+| Valor | Ordered -> Quantitative -> Sequential |    valor del credito en millones de pesos |
+
+# Why
+Tarea Principal:
+
+comparar a traves del tiempo los creditos otorgados a los pequeños y grandes productores, se espera que los grandes productores soliciten sumas mas grandes, pero es conocido que hay mas menores productores.
+
+_Query -> Compare -> Trends_ | montos otorgados a traves del tiempo
+
+
+Tarea Secundaria:
+
+Ver si existen periodos donde se otorgo un monto superiror al promedio de los creditos, esto para validar que no se dieran posibles agroingresos.
+
+_Analyze -> Consume -> Discover -> Outliers_ | valores extraños en los creditos otorgados
+
+# How
+| Encode | Manipulate | Facet | Reduce |
+| ---|---|---|---|
+|
+
+Encode -> Arrange -> Express
+Map -> Color -> hue
+Manipulate-> Change,Select
+
+# Marcas
+Lineas (definir la tendecia de las importaciones)
+Puntos (marcar la importacion en un periodo del tiempo)
+
+# Canales
+X -> Años de importacion
+Y -> Toneladas de agucatae importado
+Color -> Departamento donde llego el produto importado
--- js/main.js
+++ js/main.js
@@ -1,61 +1,44 @@
-var margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
-  },
-  width = 620,
-  height = 400,
-  radius = Math.min(width, height) / 2;
+var margin = { top: 20, right: 80, bottom: 30, left: 50 };
+(width = 620), (height = 400);
 
 //get Data
-const _urlData = "https://www.datos.gov.co/resource/5p8p-itax.json";
+const _urlData = "/data/datos.json";
 
 d3.json(_urlData).then(datos => {
   console.log(datos);
-  const _listImp = {
-    dat: d3
-      .nest()
-      .key(d => {
-        return +d.anio;
-      })
-      .rollup(d => {
-        return d3.sum(d, d => +d.vol_mentoneladas);
-      })
-      .sortKeys(d3.ascending)
-      .entries(datos),
-    categoriaDep: d3
-      .nest()
-      .key(d => {
-        return +d.departamentodestino;
-      })
-      .rollup(d => {
-        return d3.sum(d, d => +d.vol_mentoneladas);
-      })
-      .sortKeys(d3.ascending)
-      .entries(datos)
-  };
-  //console.log(_listImp.dat)
-  var _maxAnio = d3.max(_listImp.dat, d => +d.key);
-  var _minAnio = d3.min(_listImp.dat, d => +d.key);
-  var _maxTon = d3.max(_listImp.dat, d => +d.value);
-  var _minTon = d3.min(_listImp.dat, d => +d.value);
+  // const _listImp = {
+  //   dat: d3
+  //     .nest()
+  //     .key(d => {
+  //       return +d.anio;
+  //     })
+  //     .rollup(d => {
+  //       return d3.sum(d, d => +d.vol_mentoneladas);
+  //     })
+  //     .sortKeys(d3.ascending)
+  //     .entries(datos),
+  //   categoriaDep: d3
+  //     .nest()
+  //     .key(d => {
+  //       return +d.departamentodestino;
+  //     })
+  //     .rollup(d => {
+  //       return d3.sum(d, d => +d.vol_mentoneladas);
+  //     })
+  //     .sortKeys(d3.ascending)
+  //     .entries(datos)
+  // };
 
-  var xScale = d3
-    .scaleLinear()
-    .domain([_minAnio, _maxAnio]) // input
-    .range([0, width - 10]); // output
-
-  var yScale = d3
-    .scaleLinear()
-    .domain([_minTon, _maxTon]) // input
-    .range([height - 10, 0]); // output
-
+//defino como se pinta un alinea
   var line = d3
     .line()
-    .x(d => xScale(+d.key))
-    .y(d => yScale(+d.value))
-    .curve(d3.curveMonotoneX);
+    .curve(d3.curveBasis)
+    .x(function(d) {
+      return x(d.Anio);
+    })
+    .y(function(d) {
+      return y(d.Valor);
+    });
 
   //creo el espacio de trabajo
   var svg = d3
@@ -65,160 +48,177 @@ d3.json(_urlData).then(datos => {
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// defini las escalas
+  var x = d3.scaleTime().range([0, width]),
+    y = d3.scaleLinear().range([height, 0]),
+    x.domain(
+      d3.extent(datos, function(d) {
+        return d.Anio;
+      })
+    );
+    y.domain([d3.min(datos, d => d.Valor), d3.max(datos, d => d.Valor)]);
+  //Listado de Departamentos y tipos de productor
+  var departamentos = datos
+    .map(d => d.Departamento)
+    .filter((v, i, a) => a.indexOf(v) === i);
+  var tiposProductor = datos
+    .map(d => d.Tproductor)
+    .filter((v, i, a) => a.indexOf(v) === i);
+  //console.log(departamentos);
 
-  //se crea el nuevo espacio de trabajo
- 
 
-  //agrego X
-  svg
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale));
-  
-  //texto label en la linea
-  var _textLine = svg
-    .selectAll("text")
-    .enter()
-    .append("text");
-  //agrego titulos
-  //Y
-  svg
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left - 5)
-    .attr("x", 0 - height / 2)
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .text("Toneladas Importadas");
-  //X
-  svg
-    .append("text")
-    .attr(
-      "transform",
-      "translate(" + width / 2 + " ," + (height + margin.top) + ")"
-    )
-    .style("text-anchor", "middle")
-    .text("Años");
-  //Agrego Y
-  svg
-    .append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale));
+  // //se crea el nuevo espacio de trabajo
 
-  //Agrego la linea
-  svg
-    .append("path")
-    .datum(_listImp.dat)
-    .attr("class", "line")
-    .attr("d", line);
+  // //agrego X
+  // svg
+  //   .append("g")
+  //   .attr("class", "x axis")
+  //   .attr("transform", "translate(0," + height + ")")
+  //   .call(d3.axisBottom(xScale));
 
-  //Agrego los puntos
-  svg
-    .selectAll(".dot")
-    .data(_listImp.dat)
-    .enter()
-    .append("image")
-    .attr(
-      "xlink:href",
-      "https://fabianheredia.github.io/VA-importacionesAguacate/img/avocado.png"
-    )
-    .attr("x", d => {
-      let x = xScale(d.key) - 12;
-      return x;
-    })
-    .attr("y", d => {
-      //console.log(d.value)
-      let y = yScale(+d.value) - 12;
-      return y;
-    })
-    .attr("width", 24)
-    .attr("height", 24)
-    .on("click", d => {
-      //otra imagen
-      d3.select(".img2").remove();
-      svg
-        .selectAll(".dot1")
-        .data([d])
-        .enter()
-        .append("image")
-        .attr("class","img2")
-        .attr(
-          "xlink:href",
-          "https://fabianheredia.github.io/VA-importacionesAguacate/img/avocado2.png"
-        )
-        .attr("x", d => {
-          let x = xScale(d.key) - 12;
-          return x;
-        })
-        .attr("y", d => {
-          //console.log(d.value)
-          let y = yScale(+d.value) - 12;
-          return y;
-        })
-        .attr("width", 24)
-        .attr("height", 24);
-      //aqui la programacion del pie
-      const _listImpDep = {
-        dat: d3
-          .nest()
-          .key(d => {
-            return d.departamentodestino;
-          })
-          .rollup(d => {
-            return d3.sum(d, d => +d.vol_mentoneladas);
-          })
-          .sortKeys(d3.ascending)
-          .entries(datos.filter(d1 => d1.anio == d.key))
-      };
-      console.log(_listImpDep.dat);
-      update(_listImpDep.dat);
-    })
-    .on("mouseover", d => {
-      d3.select(".info").remove();
-      console.log(d);
-      svg
-        .selectAll("text.info")
-        .data([d])
-        .enter()
-        .append("text")
-        .attr("class", "info")
-        .attr("x", d => {
-          let x = xScale(d.key) + 24;
-          return x;
-        })
-        .attr("y", d => yScale(+d.value))
-        .text(d.value + " toneladas");
-    });
-  // .on("mouseout", function () {})
-  //     .on("mousemove", mousemove);
+  // //texto label en la linea
+  // var _textLine = svg
+  //   .selectAll("text")
+  //   .enter()
+  //   .append("text");
+  // //agrego titulos
+  // //Y
+  // svg
+  //   .append("text")
+  //   .attr("transform", "rotate(-90)")
+  //   .attr("y", 0 - margin.left - 5)
+  //   .attr("x", 0 - height / 2)
+  //   .attr("dy", "1em")
+  //   .style("text-anchor", "middle")
+  //   .text("Toneladas Importadas");
+  // //X
+  // svg
+  //   .append("text")
+  //   .attr(
+  //     "transform",
+  //     "translate(" + width / 2 + " ," + (height + margin.top) + ")"
+  //   )
+  //   .style("text-anchor", "middle")
+  //   .text("Años");
+  // //Agrego Y
+  // svg
+  //   .append("g")
+  //   .attr("class", "y axis")
+  //   .call(d3.axisLeft(yScale));
 
-  //    var focus = svg.append("g")
-  //        .attr("class", "focus")
-  //       .style("display", "none");
+  // //Agrego la linea
+  // svg
+  //   .append("path")
+  //   .datum(_listImp.dat)
+  //   .attr("class", "line")
+  //   .attr("d", line);
 
-  //   focus.append("circle")
-  //       .attr("r", 4.5);
+  // //Agrego los puntos
+  // svg
+  //   .selectAll(".dot")
+  //   .data(_listImp.dat)
+  //   .enter()
+  //   .append("image")
+  //   .attr(
+  //     "xlink:href",
+  //     "https://fabianheredia.github.io/VA-importacionesAguacate/img/avocado.png"
+  //   )
+  //   .attr("x", d => {
+  //     let x = xScale(d.key) - 12;
+  //     return x;
+  //   })
+  //   .attr("y", d => {
+  //     //console.log(d.value)
+  //     let y = yScale(+d.value) - 12;
+  //     return y;
+  //   })
+  //   .attr("width", 24)
+  //   .attr("height", 24)
+  //   .on("click", d => {
+  //     //otra imagen
+  //     d3.select(".img2").remove();
+  //     svg
+  //       .selectAll(".dot1")
+  //       .data([d])
+  //       .enter()
+  //       .append("image")
+  //       .attr("class","img2")
+  //       .attr(
+  //         "xlink:href",
+  //         "https://fabianheredia.github.io/VA-importacionesAguacate/img/avocado2.png"
+  //       )
+  //       .attr("x", d => {
+  //         let x = xScale(d.key) - 12;
+  //         return x;
+  //       })
+  //       .attr("y", d => {
+  //         //console.log(d.value)
+  //         let y = yScale(+d.value) - 12;
+  //         return y;
+  //       })
+  //       .attr("width", 24)
+  //       .attr("height", 24);
+  //     //aqui la programacion del pie
+  //     const _listImpDep = {
+  //       dat: d3
+  //         .nest()
+  //         .key(d => {
+  //           return d.departamentodestino;
+  //         })
+  //         .rollup(d => {
+  //           return d3.sum(d, d => +d.vol_mentoneladas);
+  //         })
+  //         .sortKeys(d3.ascending)
+  //         .entries(datos.filter(d1 => d1.anio == d.key))
+  //     };
+  //     console.log(_listImpDep.dat);
+  //     update(_listImpDep.dat);
+  //   })
+  //   .on("mouseover", d => {
+  //     d3.select(".info").remove();
+  //     console.log(d);
+  //     svg
+  //       .selectAll("text.info")
+  //       .data([d])
+  //       .enter()
+  //       .append("text")
+  //       .attr("class", "info")
+  //       .attr("x", d => {
+  //         let x = xScale(d.key) + 24;
+  //         return x;
+  //       })
+  //       .attr("y", d => yScale(+d.value))
+  //       .text(d.value + " toneladas");
+  //   });
+  // // .on("mouseout", function () {})
+  // //     .on("mousemove", mousemove);
 
-  //    focus.append("text")
-  //       .attr("x", 9)
-  //       .attr("dy", ".35em");
+  // //    var focus = svg.append("g")
+  // //        .attr("class", "focus")
+  // //       .style("display", "none");
 
-  //    svg.append("rect")
-  //        .attr("class", "overlay")
-  //       .attr("width", width)
-  //       .attr("height", height)
-  //       .on("mouseover", function() { focus.style("display", null); })
-  //       .on("mouseout", function() { focus.style("display", "none"); })
-  //       .on("mousemove", mousemove);
+  // //   focus.append("circle")
+  // //       .attr("r", 4.5);
 
-  //   function mousemove() {
-  //     var x0 = x.invert(d3.mouse(this)[0]),
-  //         i = bisectDate(data, x0, 1),
-  //         d0 = data[i - 1],
-  //         d1 = data[i],
-  //         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-  //     focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-  //     focus.select("text").text(d);
-  //   }
+  // //    focus.append("text")
+  // //       .attr("x", 9)
+  // //       .attr("dy", ".35em");
+
+  // //    svg.append("rect")
+  // //        .attr("class", "overlay")
+  // //       .attr("width", width)
+  // //       .attr("height", height)
+  // //       .on("mouseover", function() { focus.style("display", null); })
+  // //       .on("mouseout", function() { focus.style("display", "none"); })
+  // //       .on("mousemove", mousemove);
+
+  // //   function mousemove() {
+  // //     var x0 = x.invert(d3.mouse(this)[0]),
+  // //         i = bisectDate(data, x0, 1),
+  // //         d0 = data[i - 1],
+  // //         d1 = data[i],
+  // //         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+  // //     focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
+  // //     focus.select("text").text(d);
+  // //   }
 });
